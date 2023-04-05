68365953676b10dae4b8260ea7758c0737beb36a
Adam E Nagy <adam.nagy@autodesk.com>
2023-04-05 13:48:15 +0100

    forge -> aps

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ if (!config.credentials.client_id || !config.credentials.client_secret)
 let app = express();
 app.use(express.static(_path.join(__dirname, './public')));
 app.use(cookieSession({
-    name: 'forge_session',
-    keys: ['forge_secure_key'],
+    name: 'aps_session',
+    keys: ['aps_secure_key'],
     maxAge: 60 * 60 * 1000 // 1 hour, same as the 2 legged lifespan token
 }));
 app.use(express.json({
